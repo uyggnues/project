9adbcbfe35fb31fd72908096b9c36646a375cbb6
Seunggyu <seunggyul14@icloud.com>
2023-02-08 09:48:07 -0800

    cityId

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Civilian from './Civilian';
+import { useParams } from 'react-router-dom';
 
 const Game = () => {
     const [civilians, setCivilians] = useState([])
+    const params = useParams()
+    const cityId = parseInt(params.city_id)
 
     useEffect(() => {
-        fetch("http://localhost:4000/civilians")
+        fetch(`http://localhost:4000/cities/${cityId}civilians`)
         .then(resp => resp.json())
         .then(data => setCivilians(data))//setPost(data))
     }, []);
