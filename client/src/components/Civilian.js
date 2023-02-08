71372f4203594bda13cff8f76e2f3234eba6d429
Seunggyu <seunggyul14@icloud.com>
2023-02-08 09:41:41 -0800

    game

--- client/src/components/Civilian.js
+++ client/src/components/Civilian.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 const Civilian = ({civilian}) => {
+
+    const civilianHeight = Math.round(civilian.height * 10)/10
     return (
         <div className='comp'>
             <div className='left'>
                 <img className="gameImage" src={civilian.image} alt="post"/>
             </div>
             <div className='right'>
-                <div>{civilian.name}</div>
-                <div>{civilian.age}</div>
-                <div>{civilian.address}</div>
-                <div>{civilian.birthday}</div>
-                <div>{civilian.height}</div>
-                <div>{civilian.weight}</div>
+                <div className='text'>Name: {civilian.name}</div>
+                <div className='text'>Age: {civilian.age} years old</div>
+                <div className='text'>Address: {civilian.address}</div>
+                <div className='text'>Birthday: {civilian.birthday}</div>
+                <div className='text'>Height: {civilianHeight} ft</div>
+                <div className='text'>Weight: {civilian.weight} Ib</div>
+                <button className='gameCatch'>CATCH!</button>
             </div>
         </div>
     );
